f75e525691f707ce8142614ec06e09f7bcbee0d8
Zahra Alvandi <zalvandi39@gmail.com>
2026-09-19 11:26:16 +0800

    fix fullcalendar version mismatch

--- habitflow/src/Components/CalendarView.jsx
+++ habitflow/src/Components/CalendarView.jsx
@@ -2,7 +2,6 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import '@fullcalendar/react/dist/vdom';
 
 function toEvent(task) {
   const start =
