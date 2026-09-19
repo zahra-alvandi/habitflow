9d84f66e6dc1f05a6d8e331bac8265cb3f1372fa
Zahra Alvandi <zalvandi39@gmail.com>
2026-09-19 11:21:34 +0800

    update import in calendar

--- habitflow/src/Components/CalendarView.jsx
+++ habitflow/src/Components/CalendarView.jsx
@@ -2,7 +2,7 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import '@fullcalendar/core/vdom';
+import '@fullcalendar/react/dist/vdom';
 
 function toEvent(task) {
   const start =
